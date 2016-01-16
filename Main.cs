d6903a01e2480709bfae09aaeeeb6bf6903ca0ac
Michael Pollind <polli104@mail.chapman.edu>
2016-01-15 23:26:11 -0800

    start

---
+++ Main.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+namespace HelloMod
+{
+    public class Main : IMod
+    {
+        private GameObject _go;
+        
+        public string Identifier { get; set; }
+        
+        public void onEnabled()
+        {
+            _go = new GameObject();
+            _go.AddComponent<HelloBehaviour>();
+        }
+
+        public void onDisabled()
+        {
+            UnityEngine.Object.Destroy(_go);
+        }
+
+        public string Name
+        {
+            get { return "Hello Mod"; }
+        }
+
+        public string Description
+        {
+            get { return "Validates if mods are working on your PC"; }
+        }
+    }
+}
---
+++ Mod/TopHat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace HelloMod.Mod
+{
+    public class TopHat : WearableProduct
+    {
+ 
+        public TopHat()
+        {
+            this.bodyLocation = BodyLocation.HEAD;
+            
+        }
+
+        public override string getName()
+        {
+            return "TEST";
+        }
+
+        void FixedUpdate()
+        {
+            //UnityEngine.Debug.Log("tick");
+           // gObject.transform.position = this.handle.position;
+        }
+       
+    }
+}
