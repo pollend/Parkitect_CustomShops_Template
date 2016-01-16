637805199207a677f1f726cd100230f5a30298e7
Michael Pollind <mpollind@gmail.com>
2016-01-16 09:32:25 -0800

    reconfigured project

---
+++ Unity/Assets/AssetBundler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class AssetBundler : MonoBehaviour
+{
+
+	public class AssetBundleExporter
+	{
+		[MenuItem("Assets/Build AssetBundles")]
+		static void BuildAllAssetBundles()
+		{
+			BuildPipeline.BuildAssetBundles("./../assetbundle/");
+		}
+	}
+}
